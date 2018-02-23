7073aaad018ecd7877d0108b8a0a2770245425de
jingzhanwu <714107186@qq.com>
2018-02-23 14:10:47 +0800

    代码优化！

--- README.md
+++ README.md
@@ -12,7 +12,7 @@
         }
     }
 
- compile 'com.jzw.net:retrofitclient:1.1.6' 引入即可。
+ compile 'com.jzw.net:retrofitclient:1.1.7' 引入即可。
  
 
  代码中如何使用：
--- app/src/main/java/com/jzw/retrofit/base/ApiService.java
+++ app/src/main/java/com/jzw/retrofit/base/ApiService.java
@@ -2,18 +2,14 @@ package com.jzw.retrofit.base;
 
 
 import java.util.List;
-import java.util.Map;
-
 import io.reactivex.Observable;
 import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
-import retrofit2.http.PartMap;
 
 /**
  * @anthor created by Administrator
